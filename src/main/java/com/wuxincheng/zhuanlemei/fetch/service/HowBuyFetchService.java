10ff0de9daa3a255ee11f8c1f74358afa7034b02
wuxincheng <wuxinchenghappy@gmail.com>
2015-08-15 00:21:21 +0800

    开始着手基金行情的批处理

--- src/main/java/com/wuxincheng/zhuanlemei/fetch/service/HowBuyFetchService.java
+++ src/main/java/com/wuxincheng/zhuanlemei/fetch/service/HowBuyFetchService.java
@@ -1,5 +1,9 @@
 package com.wuxincheng.zhuanlemei.fetch.service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 import javax.annotation.Resource;
 
 import org.slf4j.Logger;
@@ -11,56 +15,84 @@ import com.wuxincheng.zhuanlemei.fetch.helper.HowBuyHelper;
 /**
  * 好买网信息抓取服务
  * 
- * @author wuxincheng(wxcking) 
- * @date 2015年8月14日 下午6:42:06 
- *
+ * @author wuxincheng(wxcking)
+ * @date 2015年8月14日 下午6:42:06
+ * 
  */
-@Service("HowBuyFetchService")
+@Service("howBuyFetchService")
 public class HowBuyFetchService {
 	private static final Logger logger = LoggerFactory.getLogger(HowBuyFetchService.class);
-	
+
 	@Resource
 	private HowBuyHelper howBuyHelper;
 
-	public void main(String[] args) {
-		String index = "table cellpadding=\"0\" cellspacing=\"0\" border=\"0\" width=\"100%\"";
-		String end = "div id=\"dbDivfloat\"";
+	/**
+	 * TODO 处理基金行情数据
+	 */
+	public void fundMarketInfoTask() {
+		getFundMarket();
+	}
 
-		logger.info("获取全部基金信息");
-		String fetchURL0 = "http://www.howbuy.com/fund/fundranking/";
-		howBuyHelper.fectFundInfos(fetchURL0, index, end, null);
+	/**
+	 * TODO 处理基金详细信息
+	 */
+	public void fundDetailInfoTask() {
+
+	}
+
+	/**
+	 * 获取当天所有基金行情信息
+	 * 
+	 * @return
+	 */
+	private List<Map<String, String>> getFundMarket() {
+		List<Map<String, String>> allFundMarketInfo = new ArrayList<Map<String, String>>();
+		
+		// logger.info("获取全部基金信息");
+		// String fetchURL0 = "http://www.howbuy.com/fund/fundranking/";
+		// howBuyHelper.fectFundInfos(fetchURL0, null);
 
 		logger.info("获取股票型基金信息");
 		String fetchURL1 = "http://www.howbuy.com/fund/fundranking/gupiao.htm";
-		howBuyHelper.fectFundInfos(fetchURL1, index, end, "1");
+		List<Map<String, String>> gupiao = howBuyHelper.fectFundInfos(fetchURL1, "1");
+		allFundMarketInfo.addAll(gupiao);
 
 		logger.info("获取债券型基金信息");
 		String fetchURL2 = "http://www.howbuy.com/fund/fundranking/zhaiquan.htm";
-		howBuyHelper.fectFundInfos(fetchURL2, index, end, "2");
+		List<Map<String, String>> zhaiquan = howBuyHelper.fectFundInfos(fetchURL2, "2");
+		allFundMarketInfo.addAll(zhaiquan);
 
 		logger.info("获取混合型基金信息");
 		String fetchURL3 = "http://www.howbuy.com/fund/fundranking/hunhe.htm";
-		howBuyHelper.fectFundInfos(fetchURL3, index, end, "3");
+		List<Map<String, String>> hunhe = howBuyHelper.fectFundInfos(fetchURL3, "3");
+		allFundMarketInfo.addAll(hunhe);
 
 		logger.info("获取理财型基金信息");
 		String fetchURL4 = "http://www.howbuy.com/fund/fundranking/licai.htm";
-		howBuyHelper.fectFundInfos(fetchURL4, index, end, "4");
+		List<Map<String, String>> licai = howBuyHelper.fectFundInfos(fetchURL4, "4");
+		allFundMarketInfo.addAll(licai);
 
 		logger.info("获取货币型基金信息");
 		String fetchURL5 = "http://www.howbuy.com/fund/fundranking/huobi.htm";
-		howBuyHelper.fectFundInfos(fetchURL5, index, end, "5");
+		List<Map<String, String>> huobi = howBuyHelper.fectFundInfos(fetchURL5, "5");
+		allFundMarketInfo.addAll(huobi);
 
 		logger.info("获取指数型基金信息");
 		String fetchURL6 = "http://www.howbuy.com/fund/fundranking/zhishu.htm";
-		howBuyHelper.fectFundInfos(fetchURL6, index, end, "6");
+		List<Map<String, String>> zhishu = howBuyHelper.fectFundInfos(fetchURL6, "6");
+		allFundMarketInfo.addAll(zhishu);
 
 		logger.info("获取结构型基金信息");
 		String fetchURL7 = "http://www.howbuy.com/fund/fundranking/jiegou.htm";
-		howBuyHelper.fectFundInfos(fetchURL7, index, end, "7");
+		List<Map<String, String>> jiegou = howBuyHelper.fectFundInfos(fetchURL7, "7");
+		allFundMarketInfo.addAll(jiegou);
 
 		logger.info("获取QDII基金信息");
 		String fetchURL8 = "http://www.howbuy.com/fund/fundranking/qdii.htm";
-		howBuyHelper.fectFundInfos(fetchURL8, index, end, "8");
+		List<Map<String, String>> qdii = howBuyHelper.fectFundInfos(fetchURL8, "8");
+		allFundMarketInfo.addAll(qdii);
+		
+		return allFundMarketInfo;
 	}
-	
+
 }
